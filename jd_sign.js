5a7346c8f91d63efce6d6e53513893032f62e553
Tlaeld <z398438849@outlook.com>
2022-03-03 11:28:31 +0800

    update

--- jd_sign.js
+++ jd_sign.js
@@ -37,24 +37,24 @@ async function signBeanIndex() {
     }
 }
 
-async function cgame() {
-    let url = `https://cgame-stadium.jd.com/api/v1/sign`
-    let headers = {
-        'Connection': 'keep-alive',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Content-Type': 'application/json',
-        'Origin': 'https://pro.m.jd.com',
-        'ActivityId': '7c51826be9f241c1ad9733df34d242c5',
-        'Host': 'cgame-stadium.jd.com',
-        'Referer': 'https://pro.m.jd.com/mall/active/dj6us2JJRLMMBb4iDaSK4wxvBMt/index.html',
-        'Accept-Language': 'zh-cn',
-        'Accept': 'application/json',
-        'Cookie': $.cookie
-    }
-    let data = await $.post(url, {}, headers)
-    let bean = data?.data?.beanNum || 0;
-    if (bean > 0) {
-        $.log(`汽车签到 获得${bean}京豆`)
-    }
-}
+// async function cgame() {
+//     let url = `https://cgame-stadium.jd.com/api/v1/sign`
+//     let headers = {
+//         'Connection': 'keep-alive',
+//         'Accept-Encoding': 'gzip, deflate, br',
+//         'Content-Type': 'application/json',
+//         'Origin': 'https://pro.m.jd.com',
+//         'ActivityId': '7c51826be9f241c1ad9733df34d242c5',
+//         'Host': 'cgame-stadium.jd.com',
+//         'Referer': 'https://pro.m.jd.com/mall/active/dj6us2JJRLMMBb4iDaSK4wxvBMt/index.html',
+//         'Accept-Language': 'zh-cn',
+//         'Accept': 'application/json',
+//         'Cookie': $.cookie
+//     }
+//     let data = await $.post(url, {}, headers)
+//     let bean = data?.data?.beanNum || 0;
+//     if (bean > 0) {
+//         $.log(`汽车签到 获得${bean}京豆`)
+//     }
+// }
 
