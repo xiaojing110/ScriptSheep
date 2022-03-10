a972a96d2f3ce516a7bf10a2884600544638c35a
Tlaeld <z398438849@outlook.com>
2022-03-10 09:02:48 +0800

    Update jd_wx_luckDraw.js

--- jd_wx_luckDraw.js
+++ jd_wx_luckDraw.js
@@ -10,7 +10,7 @@ cron "10 10 10 10 0" script-path=jd_wx_luckDraw.js,tag=幸运抽奖
 let mode = __dirname.includes('/home/magic/Work/wools/magic/raw')
 const {Env} = mode ? require('../magic') : require('./magic')
 const $ = new Env('M幸运抽奖');
-$.lz = 'LZ_TOKEN_KEY=lztokef1eb8494b0af868bd18bdaf8;LZ_TOKEN_VALUE=Aa5RE8RuY4X3zA==;';
+$.lz = 'LZ_TOKEN_KEY=lztokenpage7a904cedc8e6496a807ca19bce8902ff;LZ_TOKEN_VALUE=B0oH3vDY2bhHWuKbVaJ+XQ==;';
 $.activityUrl = process.env.M_WX_LUCK_DRAW_URL ? process.env.M_WX_LUCK_DRAW_URL
     : '';
 if (mode) {
