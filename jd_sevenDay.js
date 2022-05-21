ab7ece8c2423239733f0500fd77d34fe635fd537
Tlaeld <z398438849@outlook.com>
2022-05-21 15:10:27 +0800

    update

--- jd_sevenDay.js
+++ jd_sevenDay.js
@@ -18,56 +18,19 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [], cookie = '', message = '';
 // https://lzkj-isv.isvjcloud.com/sign/sevenDay/signActivity?activityId=
-let activityIdList = [
-    '323f8b4d8d5d45f98332c669169d4bec',
-    '93ec92b03d144314b051e57f4e2beab3',
-    '154849de8f394afc8ff6f45742643226',
-    '160d7d10d95f43e598fcb9d60107438d', 
-    '8134d9aaaf304eec97486b931f384df5', 
-    'a5813c5465954aa187d720ce2eeac26f', 
-    '1acc06bc657c4c019cf237d7dc2a373a', 
-    '64c32fa993e94f77ba9a2cab34584199', 
-    'fb8cbc0f6de34e0d9f33f4bcc0fca33d', 
-    'b224f1b7de3e4472ac9a39d424a272e8'
+activityIdList =  [
+    'd3f04569638a4a3399feb878ddfb9755', 
+    'bf1f97a0180b4a159a00431a6ad6ef54'
 ]
 // https://lzkj-isv.isvjcloud.com/sign/signActivity2?activityId=
 let activityIdList2 = [
-    '113b9227034341f0956fe7f299e5274f',
-    'aac7a693623b430e9faf688a7fc20085', 
-    'e0bb90e2ee4d44128808b49cce8256f9', 
-    '97d41549077d4748ac794ae4b1019d25', 
-    'dc11ef2f624c4d30bfd4bf59bfb8cf7e', 
-    '6d957ee1f7224a819163bda196f69dcf', 
     'e80ac40515e844c8a0183d2dd46ba17f', 
-    'ae8a435382ea441889ab9f2c46336822', 
-    '2daf7482d15a43cfac4aa5579ab5b1ae', 
-    '3dd4f710fd644963813a2d27ef77550f', 
-    '9eedfba8f9b44397b420a47615ef45da', 
-    '536b0ee5161748e49afdb074c5e4e2a9', 
-    '5ced0d5e28554c21897644eb344217b2', 
-    '761a61eea8494d4a94ef7a8b061959e5', 
-    '6870035682524fd69a9eba4dc2e2e038'
+    'd3e0695734f441a8b6a33fcefc6a1cfc'
 ]
 // https://cjhy-isv.isvjcloud.com/sign/signActivity?activityId=
 let activityIdList3 = [
-    'd1652484acc24e01b6aee1c2d0179ff2',
-    '3dd3589be4cb4a2582a4e3227ba4e962',
-    '810023f647c440cc8e90867bfd6b619f', 
-    'b6783d89a4a747aa8ddd3db68e4faff2', 
-    'bb35aad5d8d047b198c81a8c3585b7fe', 
-    '5c24acfa02804fb4a21b2882f5b9c447', 
-    '138af09def0e43299a377ca97635c250', 
-    'cf816a1d9e6d4f91b9301c580f3dcebd', 
-    'edf2837ae237408c9e7e830da86a5d83', 
-    '7516daaaeb4040bc8695849b14f0e51f', 
-    '71cd16775a7844aa89f036006c5b8306', 
-    '74e5151341e34a35a13a926a01659465', 
-    '027857eee3444da4ac722d11eb221fcf', 
-    'a025933ecbbc455498e93aa6be4c7155', 
-    '8ee051adb69541ea8d3324e1d0b7acc9', 
-    '7dd0cfe95ea946d1a29f094c6f433060', 
-    '5f3679eb7c154bf6a99adc35bf93fc5e', 
-    '554465cd56b04b98873ac22b19d9307b'
+    '0e251bd8dec64f7dafd8f2e0d201f3f3',
+    '249c64be0e714045adc2fe86abb5ba9e',
 ]
 let lz_cookie = {}
 
