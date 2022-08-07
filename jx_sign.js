a170c5bbeea342820e2043ab5601a0bf9f0df18c
Tlaeld <z398438849@outlook.com>
2022-08-07 15:25:43 +0800

    update

--- jx_sign.js
+++ jx_sign.js
@@ -65,7 +65,7 @@ if ($.isNode()) {
       
       } else {
         if (!isLoginInfo[$.UserName]) {
-          
+          await TotalBean();
           isLoginInfo[$.UserName] = $.isLogin
         }
       }
@@ -92,7 +92,7 @@ if ($.isNode()) {
       
       } else {
         if (!isLoginInfo[$.UserName]) {
-          
+          await TotalBean();
           isLoginInfo[$.UserName] = $.isLogin
         }
       }
@@ -366,16 +366,14 @@ function dotask(task) {
 
 // 宝箱
 function bxdraw() {
-  let functionId, body;
+  let body;
   if ($.signhb_source === '5') {
-    functionId = "signhb/bxdraw_jxpp"
     body = `ispp=1&sqactive=${$.sqactive}&tk=`
   } else {
-    functionId = "signhb/bxdraw"
     body = `ispp=0&sqactive=&tk=`
   }
   return new Promise((resolve) => {
-    $.get(taskUrl(functionId, body), async (err, resp, data) => {
+    $.get(taskUrl("signhb/bxdraw", body), async (err, resp, data) => {
       try {
         if (err) {
           console.log(JSON.stringify(err));
